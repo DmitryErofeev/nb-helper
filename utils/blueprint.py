db05f9064ff2d42293b665a5b5672485ee6c84e2
erofeev <erofeev@smartintel.ru>
2021-03-15 14:06:17 +0300

    добавлена кнопка- удалить линк коммутатора из Нетбокса

--- utils/blueprint.py
+++ utils/blueprint.py
@@ -1,12 +1,20 @@
+from flask.globals import session
 import requests
-from flask import Blueprint, render_template, request
+from flask import Blueprint, flash, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.fields.core import SelectField
 import utils
+import pynetbox
+import os
 from cache import cache
 from config import MAC_API_URL
 
+
+nb_url = os.getenv('NETBOX_URL')
+nb_token = os.getenv('NETBOX_TOKEN')
+
+nb = pynetbox.api(nb_url, nb_token)
 
 add_utils = Blueprint('add_utils', __name__, template_folder='templates')
 
@@ -45,6 +53,65 @@ class GetLink(FlaskForm):
     submit = SubmitField('Удалить линк:')
 
 
+@add_utils.route('/del_cable_step1', methods=['GET'])
+def get_region():
+    form = GetREgion()
+
+    form.get_region.choices = [(region.slug, ':'.join([str(region.parent), str(region.name)]))
+                                for region in nb.dcim.regions.all()
+                                if region.parent
+                                ]
+
+    return render_template('utils/step1.html', form=form)
+
+
+@add_utils.route('del_cable_step2', methods=['POST'])
+def get_site():
+    form = GetSite()
+
+    session['region'] = request.form.get('get_region')
+    sites = nb.dcim.sites.filter(region=session['region'])
+    form.get_site.choices = [site.slug for site in sites]
+
+    return render_template('utils/step2.html', form=form)
+
+
+@add_utils.route('del_cable_step3', methods=['POST'])
+def get_device():
+    form = GetDevice()
+
+    session['site'] = request.form.get('get_site')
+    devices = nb.dcim.devices.filter(site=session['site'])
+    form.get_device.choices = [device for device in devices]
+
+    return render_template('utils/step3.html', form=form)
+
+
+@add_utils.route('del_cable_step4', methods=['POST'])
+def get_cable():
+    form = GetLink()
+
+    session['device'] = request.form.get('get_device')
+    _device = nb.dcim.devices.get(name=session['device'])
+    _interfaces = nb.dcim.interfaces.filter(device_id=_device.id, cabled=True)
+
+    form.get_cable_link.choices = [link_id.cable_peer.cable for link_id in _interfaces]
+
+    return render_template('utils/step4.html', form=form)
+
+
+@add_utils.route('del_cable_step5', methods=['POST'])
+def del_cable():
+    session['cable'] = request.form.get('get_cable_link')
+    # _device = nb.dcim.devices.get(name=session['device'])
+    _cable = nb.dcim.cables.get(session['cable'])
+    try:
+        _cable.delete()
+        flash('Линк удален', category='success')
+    except:
+        flash('Ошибка', category='danger')
+
+    return render_template('utils/step5.html',nb_url=nb_url, device=_device)
 
 
 @add_utils.route('/utils-slug', methods=['POST', 'GET'])
